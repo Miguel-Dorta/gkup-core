105f8bd561879b786165576b92dd4e05a2390bfd
Miguel-Dorta <contact@migueldorta.com>
2020-04-21 19:01:56 +0100

    Add more hash algorithms

--- pkg/hash/hasher.go
+++ pkg/hash/hasher.go
@@ -1,22 +1,31 @@
 package hash
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 	"github.com/Miguel-Dorta/gkup-core/pkg/common"
+	"golang.org/x/crypto/sha3"
 	"hash"
 	"io"
 	"os"
 )
 
 type Hasher struct {
-	h hash.Hash
+	h   hash.Hash
 	buf []byte
 }
 
 const (
-	SHA256 = "sha256"
+	MD5      = "md5"
+	SHA1     = "sha1"
+	SHA256   = "sha256"
+	SHA512   = "sha512"
+	SHA3_256 = "sha3-256"
+	SHA3_512 = "sha3-512"
 
 	minimumBufferSize = 512
 )
@@ -24,8 +33,18 @@ const (
 func NewHasher(hashAlgorithm string, bufSize int) *Hasher {
 	var h hash.Hash
 	switch hashAlgorithm {
+	case MD5:
+		h = md5.New()
+	case SHA1:
+		h = sha1.New()
 	case SHA256:
 		h = sha256.New()
+	case SHA512:
+		h = sha512.New()
+	case SHA3_256:
+		h = sha3.New256()
+	case SHA3_512:
+		h = sha3.New512()
 	default:
 		panic("invalid hash algorithm: " + hashAlgorithm)
 	}
